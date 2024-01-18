f2d2b6a72a70111ae7adc88d5fadd738f0b01466
hassanrnt <hassanritouny97@gmail.com>
2024-01-18 16:35:41 +0100

    adding the product context and building the routing system

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -7,15 +7,21 @@ const ProductProvider= ({children}) => {
   // product state
   const[products, setProduct] = useState([]);
   // fetch product
+  const fetchProducts = async() =>{
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    setProduct(data);
+  };
   useEffect(()=>{
-    const fetchProducts = async() =>{
-      const response = await fetch('https://fakestoreapi.com/products');
-      const data = await response.json();
-      setProduct(data);
-    };
+   
     fetchProducts();
   },[]);
-  return <ProductContext.Provider value={{products}}>{children}</ProductContext.Provider>;
+  return (
+    <ProductContext.Provider value={{products}}>
+        {children}
+    </ProductContext.Provider>
+  )
+ 
 };
 
 export default ProductProvider;
